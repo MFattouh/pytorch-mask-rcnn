331ccd46b20d96123a2b74dcd634448acb93e374
Mohammad Fattouh <mo.fattouh@gmail.com>
2018-07-05 15:13:06 +0200

    refactor code. add empty string to compensate unused labels.

--- demo.py
+++ demo.py
@@ -15,6 +15,22 @@ import visualize
 import torch
 
 
+class_names = ['BG', 'person', 'bicycle', 'car', 'motorcycle', 'airplane',
+               'bus', 'train', 'truck', 'boat', 'traffic light',
+               'fire hydrant', '', 'stop sign', 'parking meter', 'bench', 'bird',
+               'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear',
+               'zebra', 'giraffe', '', 'backpack', 'umbrella', '', '', 'handbag', 'tie',
+               'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball',
+               'kite', 'baseball bat', 'baseball glove', 'skateboard',
+               'surfboard', 'tennis racket', 'bottle', '', 'wine glass', 'cup',
+               'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple',
+               'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza',
+               'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed', '',
+               'dining table', '', '', 'toilet', '', 'tv', 'laptop', 'mouse', 'remote',
+               'keyboard', 'cell phone', 'microwave', 'oven', 'toaster',
+               'sink', 'refrigerator', '', 'book', 'clock', 'vase', 'scissors',
+               'teddy bear', 'hair drier', 'toothbrush']
+
 # Root directory of the project
 ROOT_DIR = os.getcwd()
 
@@ -29,6 +45,7 @@ COCO_MODEL_PATH = os.path.join(ROOT_DIR, "mask_rcnn_coco.pth")
 # Directory of images to run detection on
 IMAGE_DIR = os.path.join(ROOT_DIR, "images")
 
+
 class InferenceConfig(coco.CocoConfig):
     # Set batch size to 1 since we'll be running inference on
     # one image at a time. Batch size = GPU_COUNT * IMAGES_PER_GPU
@@ -36,45 +53,37 @@ class InferenceConfig(coco.CocoConfig):
     GPU_COUNT = 1
     IMAGES_PER_GPU = 1
 
-config = InferenceConfig()
-config.display()
 
-# Create model object.
-model = modellib.MaskRCNN(model_dir=MODEL_DIR, config=config)
-if config.GPU_COUNT:
-    model = model.cuda()
+def main():
 
-# Load weights trained on MS-COCO
-model.load_state_dict(torch.load(COCO_MODEL_PATH))
+    config = InferenceConfig()
+    config.display()
 
-# COCO Class names
-# Index of the class in the list is its ID. For example, to get ID of
-# the teddy bear class, use: class_names.index('teddy bear')
-class_names = ['BG', 'person', 'bicycle', 'car', 'motorcycle', 'airplane',
-               'bus', 'train', 'truck', 'boat', 'traffic light',
-               'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird',
-               'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear',
-               'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie',
-               'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball',
-               'kite', 'baseball bat', 'baseball glove', 'skateboard',
-               'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup',
-               'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple',
-               'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza',
-               'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed',
-               'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote',
-               'keyboard', 'cell phone', 'microwave', 'oven', 'toaster',
-               'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors',
-               'teddy bear', 'hair drier', 'toothbrush']
+    # Create model object.
+    model = modellib.MaskRCNN(model_dir=MODEL_DIR, config=config)
+    if config.GPU_COUNT:
+        model = model.cuda()
 
-# Load a random image from the images folder
-file_names = next(os.walk(IMAGE_DIR))[2]
-image = skimage.io.imread(os.path.join(IMAGE_DIR, random.choice(file_names)))
+        # Load weights trained on MS-COCO
+        model.load_state_dict(torch.load(COCO_MODEL_PATH))
 
-# Run detection
-results = model.detect([image])
+        # COCO Class names
+        # Index of the class in the list is its ID. For example, to get ID of
+        # the teddy bear class, use: class_names.index('teddy bear')
 
-# Visualize results
-r = results[0]
-visualize.display_instances(image, r['rois'], r['masks'], r['class_ids'],
-                            class_names, r['scores'])
-plt.show()
+        # Load a random image from the images folder
+        file_names = next(os.walk(IMAGE_DIR))[2]
+        image = skimage.io.imread(os.path.join(IMAGE_DIR, random.choice(file_names)))
+
+        # Run detection
+        results = model.detect([image])
+
+        # Visualize results
+        r = results[0]
+        visualize.display_instances(image, r['rois'], r['masks'], r['class_ids'],
+                                    class_names, r['scores'])
+        plt.show()
+
+
+if __name__ == '__main__':
+    main()
